00b91d452993dede0a295928247fc179846669f7
Uwande Thomás <61985599+uwandera@users.noreply.github.com>
2022-05-26 13:28:05 -0300

    Video full

--- js/monster.js
+++ js/monster.js
@@ -57,3 +57,35 @@ console.clear();*/
 
 //instantiate GSDevTools with default settings
 // GSDevTools.create( );
+
+
+/* jQuery library for video fullscreen
+
+<></>
+
+var fullscreen = 0;
+
+$(".fullscreen").click(function(){
+  if(fullscreen == 0){
+    fullscreen = 1;
+    $("video").appendTo('body');
+    $("#vidControls").appendTo('body');
+    $("video").css('position', 'absolute').css('width', '100%').css('height', '90%').css('margin', 0).css('margin-top', '5%').css('top', '0').css('left', '0').css('float', 'left').css('z-index', 600);
+    $("#vidControls").css('position', 'absolute').css('bottom', '5%').css('width', '90%').css('backgroundColor', 'rgba(150, 150, 150, 0.5)').css('float', 'none').css('left', '5%').css('z-index', 700).css('-webkit-border-radius', '10px');
+
+}
+else
+    {
+
+        fullscreen = 0;
+
+        $("video").appendTo('#videoPlayer');
+        $("#vidControls").appendTo('#videoPlayer');
+
+        //change <video> css back to normal
+
+        //change "#vidControls" css back to normal
+
+    }
+
+});
